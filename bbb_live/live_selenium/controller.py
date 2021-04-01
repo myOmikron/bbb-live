eb5150c35223f1775c6eb67a3c3f0ceca5148035
myOmikron <accounts@omikron.dev>
2021-04-01 22:57:36 +0200

    Added baseline converting to ffmpeg magic string

--- bbb_live/live_selenium/controller.py
+++ bbb_live/live_selenium/controller.py
@@ -72,7 +72,7 @@ class Streamer:
                 break
 
         logger.info("Start ffmpeg")
-        cmd = f'ffmpeg -thread_queue_size 1024 -framerate 30 -f x11grab -draw_mouse 0 -s 1920x1080 -i :{display} -thread_queue_size 1024 -f pulse -i default -ac 1 -c:a aac -b:a 160k -ar 44100 -threads 0 -c:v libx264 -f flv "{stream_address}"'
+        cmd = f'ffmpeg -thread_queue_size 1024 -framerate 30 -f x11grab -draw_mouse 0 -s 1920x1080 -i :{display} -thread_queue_size 1024 -f pulse -i default -ac 1 -c:a aac -b:a 160k -ar 44100 -threads 0 -c:v libx264 -profile:v baseline -pix_fmt yuv420p -f flv "{stream_address}"'
         cmd = shlex.split(cmd)
         self.process = subprocess.Popen(cmd)
         self.process.communicate()
